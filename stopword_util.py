f6f9ce4839207cf5ed7cc9faf4b7119f3ca765bc
fmx <1417388068@qq.com>
2019-03-30 22:23:21 -0700

    stopword_utils

---
+++ stopword_util.py
@@ -0,0 +1,27 @@
+import jieba
+import sys
+import codecs
+
+def getStopwords():
+    stopword_file = "stop_words_source/chinese_stop_words.txt"
+    stopwords = {}
+    for word in codecs.open(stopword_file, 'r', encoding='utf-8').readlines():
+        stopwords.add(word.strip())
+    return stopwords
+
+def preprocessSen(line, stopwords):
+    words = jieba.cut(line, cut_all=False)
+    sen = ""
+    for word in words:
+        word = word.strip()
+        if word not in stopwords:
+            sen += word + " "
+    return sen
+
+def preprocessCorpus(corpus, stopwords):
+    txt = corpus.review
+    processed_txt = ""
+    for line in txt:
+        processed_line = preprocessSen(line, stopwords)
+        processed_txt += processed_line + '\n'
+    return processed_txt
